a03856a3294e84f04825c1aed07d8c6c75428056
kevalsakhiya <kevalsakhiya@gmail.com>
2022-12-04 18:22:50 +0530

    ended the first chapter

--- blog_website/mysite/blog/urls.py
+++ blog_website/mysite/blog/urls.py
@@ -5,5 +5,5 @@ app_name = 'blog'
 # A URL pattern is composed of a string pattern, a view, and, optionally, a name that allows you to name the URL project-wide.
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:id>/'), views.post_detail, name='post_detail'
+    path('<int:id>/', views.post_detail, name='post_detail')
 ***REMOVED***
