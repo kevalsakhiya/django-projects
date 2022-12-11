73744f93c17daa459a03fa9cec5b43fdd4c022f9
kevalsakhiya <kevalsakhiya@gmail.com>
2022-12-11 12:59:53 +0530

    made slug field unique by applying unique_for_date in models.py

--- blog_website/mysite/blog/models.py
+++ blog_website/mysite/blog/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User,
                                 on_delete=models.CASCADE,
                                 related_name='blog_posts')
