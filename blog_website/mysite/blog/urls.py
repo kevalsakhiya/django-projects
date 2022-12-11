76b8e8e2a09802affe59903c2c78f043dfe4f299
kevalsakhiya <kevalsakhiya@gmail.com>
2022-12-11 13:11:32 +0530

    Modified the path for post detail in urls.py

--- blog_website/mysite/blog/urls.py
+++ blog_website/mysite/blog/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path 
 from . import views
 
 app_name = 'blog'
 # A URL pattern is composed of a string pattern, a view, and, optionally, a name that allows you to name the URL project-wide.
 urlpatterns = [
+    # param of path() => https://docs.djangoproject.com/en/4.1/intro/tutorial01/#write-your-first-view
     path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail')
+    path('<int:year>/<int:month>/<int:day>/<slug:post>',
+         views.post_detail, 
+         name='post_detail')
 ***REMOVED***
