ab5768290f964297242ccdec821ed749d4f97c42
kevalsakhiya <kevalsakhiya@gmail.com>
2022-12-11 14:12:32 +0530

    logic for pagination added

--- blog_website/mysite/blog/views.py
+++ blog_website/mysite/blog/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+# from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
     '''Returns all published posts'''
-    posts  =Post.published.all()
+    post_list = Post.published.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list,3)
+    page_number = request.GET.get('page',1) # .get() returns 1 if there is no page given
+   
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+
     return render(request,
                     'blog/post/list.html',
                 ***REMOVED***'posts':posts***REMOVED***)
