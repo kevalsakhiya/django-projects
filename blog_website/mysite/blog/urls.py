784c198f47a98f5940177d6c3102fa8cdb6a6d29
kevalsakhiya <kevalsakhiya@gmail.com>
2022-12-11 14:47:17 +0530

    added class based views, and commented after testing

--- blog_website/mysite/blog/urls.py
+++ blog_website/mysite/blog/urls.py
@@ -5,7 +5,10 @@ app_name = 'blog'
 # A URL pattern is composed of a string pattern, a view, and, optionally, a name that allows you to name the URL project-wide.
 urlpatterns = [
     # param of path() => https://docs.djangoproject.com/en/4.1/intro/tutorial01/#write-your-first-view
-    path('', views.post_list, name='post_list'),
+
+    path('', views.post_list, name='post_list'),     # function based listview
+    # as_view is the function(class method) which will connect my MyView class with its url.
+    # path('',views.PostListView.as_view(), name='post_list'),  # class based post view
     path('<int:year>/<int:month>/<int:day>/<slug:post>',
          views.post_detail, 
          name='post_detail')
--- blog_website/mysite/blog/views.py
+++ blog_website/mysite/blog/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 # from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
+class PostListView(ListView):
+    ***REMOVED***
+    Alternative post list view
+    ***REMOVED***
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_list(request):
     '''Returns all published posts'''
