7c394f85757568ade74e3afe1306844abad77eec
kevalsakhiya <kevalsakhiya@gmail.com>
2023-01-12 15:38:43 +0530

    added functionality to show similar posts
    this functionality takes all the tags attached with the post and render the similar post that has same tags to it

--- blog_website/mysite/blog/views.py
+++ blog_website/mysite/blog/views.py
@@ -7,7 +7,7 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
-
+from django.db.models import Count
 
 class PostListView(ListView):
     ***REMOVED***
@@ -60,11 +60,17 @@ def post_detail(request,year,month,day,post):
     # Form for users to comment
     form = CommentForm()
 
+    # List of similar post
+    post_tags_ids = post.tags.values_list('id',flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4***REMOVED***
     return render(request,
                     'blog/post/detail.html',
                 ***REMOVED***'post':post,
                     'comments': comments,
-                    'form': form***REMOVED***,             
+                    'form': form,
+                    'similar_posts':similar_posts,
+            ***REMOVED***
                     )
 
 def post_share(request, post_id):
