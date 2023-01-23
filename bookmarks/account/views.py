49ff5fa2470f6d2745753165ea29abcc216f12f7
kevalsakhiya <kevalsakhiya@gmail.com>
2023-01-23 12:17:26 +0530

    login and logout using django authentication views

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 
 def user_login(request):
@@ -29,3 +30,11 @@ def user_login(request):
         form = LoginForm()
     
     return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section':'dashboard'}
+    )
