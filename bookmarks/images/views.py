6a2312fd4b50ad5b25c44731f14839730bb8662b
kevalsakhiya <kevalsakhiya@gmail.com>
2023-02-01 14:56:43 +0530

    Chepter-6 finished, Adding infinite page functionality

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, \
+                                  PageNotAnInteger
 from .forms import ImageCreateForm
 from .models import Image
-from django.http import JsonResponse, HttpResponse
-from django.views.decorators.http import require_POST
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 @login_required
@@ -19,28 +22,25 @@ def image_create(request):
             new_image = form.save(commit=False)
             # assign current user to the item
             new_image.user = request.user
-            # commit is set to True by default so now it will save the image
             new_image.save()
-            messages.success(request,'Image added successfully')
-            # redirect to new created item detail view
+            messages.success(request, 'Image added successfully')
+            # redirect to new created image detail view
             return redirect(new_image.get_absolute_url())
-
     else:
         # build form with data provided by the bookmarklet via GET
         form = ImageCreateForm(data=request.GET)
-
     return render(request,
                   'images/image/create.html',
-                  {'section': 'images','form': form}
-                  )
+                  {'section': 'images',
+                   'form': form})
 
 
-def image_detail(request,id,slug):
+def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request,
                   'images/image/detail.html',
-                  {'section': 'images','image': image}
-                   )
+                  {'section': 'images',
+                   'image': image})
 
 
 @login_required
@@ -48,7 +48,6 @@ def image_detail(request,id,slug):
 def image_like(request):
     image_id = request.POST.get('id')
     action = request.POST.get('action')
-
     if image_id and action:
         try:
             image = Image.objects.get(id=image_id)
@@ -62,14 +61,12 @@ def image_like(request):
     return JsonResponse({'status': 'error'})
 
 
-
 @login_required
 def image_list(request):
     images = Image.objects.all()
-    paginator = Paginator(Image,8)
+    paginator = Paginator(images, 8)
     page = request.GET.get('page')
     images_only = request.GET.get('images_only')
-
     try:
         images = paginator.page(page)
     except PageNotAnInteger:
@@ -81,14 +78,13 @@ def image_list(request):
             # return an empty page
             return HttpResponse('')
         # If page out of range return last page of results
-    
+        images = paginator.page(paginator.num_pages)
     if images_only:
         return render(request,
                       'images/image/list_images.html',
-                      {'section': 'images','images': images}
-                      )
-    
+                      {'section': 'images',
+                       'images': images})
     return render(request,
-                      'images/image/list_images.html',
-                      {'section': 'images','images': images}
-                      )
+                  'images/image/list.html',
+                   {'section': 'images',
+                    'images': images})
