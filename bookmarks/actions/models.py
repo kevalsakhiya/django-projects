c9b77504041a1666c3fa9226d4b11b1380a467a0
kevalsakhiya <kevalsakhiya@gmail.com>
2023-02-06 18:35:16 +0530

    adding generic relationship between models

--- bookmarks/actions/models.py
+++ bookmarks/actions/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 class Action(models.Model):
     user = models.ForeignKey('auth.User',
@@ -7,9 +10,18 @@ class Action(models.Model):
     )
     verb = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
+    target_ct = models.ForeignKey(ContentType,
+                                  blank=True,
+                                  null=True,
+                                  related_name='target_obj',
+                                  on_delete=models.CASCADE
+    )
+    target_id = models.PositiveBigIntegerField(null=True,blank=True)
+    target = GenericForeignKey('target_ct','target_id')
 
     class Meta:
         indexes = [
             models.Index(fields=['-created']),
+            models.Index(fields=['target_ct','target_id'])
         ]
         ordering = ['-created']
