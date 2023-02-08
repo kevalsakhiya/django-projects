d539638416a264eccfce26177695ceb58fe284d2
kevalsakhiya <kevalsakhiya@gmail.com>
2023-02-08 17:05:55 +0530

    feed functionality added

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from .models import Contact
+from actions.utils import create_action
+from actions.models import Action
 
 
 def user_login(request):
@@ -41,9 +43,20 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
+    # Display all actions by default
+    # retrieve all actions from the database, excluding the ones performed by the current user.
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id',flat=True)
+    
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    
+    actions = actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10]
+    
     return render(request,
                   'account/dashboard.html',
-                  {'section':'dashboard'}
+                  {'section':'dashboard','actions': actions}
     )
 
 
@@ -59,6 +72,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user}
@@ -133,6 +147,7 @@ def user_follow(request):
                     user_from = request.user,
                     user_to = user
                 )
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user,
                                        user_to=user).delete()
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -9,6 +9,7 @@ from django.core.paginator import Paginator, EmptyPage, \
                                   PageNotAnInteger
 from .forms import ImageCreateForm
 from .models import Image
+from actions.utils import create_action
 
 
 @login_required
@@ -23,6 +24,7 @@ def image_create(request):
             # assign current user to the item
             new_image.user = request.user
             new_image.save()
+            create_action(request.user,'bookmarked image', new_image)
             messages.success(request, 'Image added successfully')
             # redirect to new created image detail view
             return redirect(new_image.get_absolute_url())
@@ -53,6 +55,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
