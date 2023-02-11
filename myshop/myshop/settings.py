efe512bb139f8172868211f8f25fd7a014568183
kevalsakhiya <kevalsakhiya@gmail.com>
2023-02-11 17:12:43 +0530

    chapter-8 finishhed, functionality of order added, asynchronous functionality with celery and rabbitMQ

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 CART_SESSION_ID = 'cart'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
