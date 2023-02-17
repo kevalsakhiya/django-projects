639fb9742a7a66ba148cc130df9be3a6254d032c
kevalsakhiya <kevalsakhiya@gmail.com>
2023-02-17 15:06:57 +0530

    chapter 15  finished, views useing REST API

---
+++ api_examples/enroll_all.py
@@ -0,0 +1,22 @@
+import requests
+
+username = 'test2'
+password = 'monkeydance'
+
+
+base_url = 'http://127.0.0.1:8000/api/'
+
+# retrieve all courses
+r = requests.get(f'{base_url}courses/')
+courses = r.json()
+
+available_courses = ', '.join([course['title'] for course in courses])
+print(f'Available courses: {available_courses}')
+
+for course in courses:
+    course_id = course['id']
+    course_title = course['title']
+    r = requests.post(f'{base_url}courses/{course_id}/enroll/',auth=(username, password))
+
+    if r.status_code == 200:
+        print(f'Successfully enrolled in {course_title}')
